6f6a09c82f06200aeecb4d0f85d9a0231dc9938b
virghiu <virghiu@gmail.com>
2023-08-07 19:53:07 -0400

    feat: complete file metadata

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile') , async (req, res) => {
+    try {
+      const file = req.file;
+      if (!file) {
+        return res.status(402).json({ message: 'The upfile field is required.' });
+      }
+      console.log(file);
+
+      res.json({
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.size
+      });
+
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
 
 
 
